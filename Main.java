2a7c11cca8e8e7091f5297c5e79031035dad2580
Gormi3815 <gormi3815@wrdsb.ca>
2020-10-15 01:18:00 +0000

    this program will tell the user their letter grade based off of their number grade

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
-/**
+import java.util.Scanner;
+/** This program will ask the user for their number grade and then give them a letter grade
  *
- * @author 
+ * @author Isaac
  */
 public class Main {
 
@@ -8,8 +9,30 @@ public class Main {
    * The method that is executed when you hit the run button.
    * @param args the command line arguments
    */
+   public static void examGrade(){
+     Scanner input = new Scanner (System.in);
+    System.out.println("Please enter your number grade");
+    int grade = input.nextInt(); 
+    if (grade <= 50){
+      System.out.println("F");
+    } else if (grade <= 59){
+      System.out.println("D");
+    } else if (grade <= 69){
+      System.out.println("C");
+    } else if (grade <= 79){
+     System.out.println ("B");
+    } else if (grade <=100){
+      System.out.println("A");
+    }
+       
+       }
+     
+   
+
+
   public static void main(String[] args) {
     // TODO code application logic here
-    
+     
+   examGrade();
   }
 }
